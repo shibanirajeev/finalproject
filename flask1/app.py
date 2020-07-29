6450e5c02db959ab287184ec7c76cf311c1e590e
LEONDATA20 <HANGLI1118@GMAIL.COM>
2020-07-28 22:59:48 -0400

    flask

--- flask1/app.py
+++ flask1/app.py
@@ -6,7 +6,7 @@ from textblob import TextBlob
 import sklearn.externals
 import joblib
 
-app = Flask(__name__) 
+app = Flask(__name__, static_url_path="") 
 
 scaler = joblib.load( "scaler.save")
 modelIE = load_model("modelIE.h5")
@@ -14,16 +14,21 @@ modelNS = load_model("model_testNS.h5")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/background')
-def background():
     return render_template('background.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 @app.route('/analysis')
 def analysis():
     return render_template('analysis.html')
 
+@app.route("/image/<path:path>")
+def image(path):
+    return send_from_directory('templates/', path)
+
+#??? why will go to 
 @app.route('/predict',methods=['POST', 'GET'])
 def predict():
     if request.method == 'GET':
@@ -42,6 +47,7 @@ def predict():
         df_test['ellipsis_per_comment'] = df_test['posts'].apply(lambda x: x.count('...')/50)
         Xx = df_test.drop(['posts'], axis=1)
         X_features = scaler.transform(Xx)
+        
         predictionIE = modelIE.predict(X_features)
         ur_comment1 = f'Answer1: {input_features[0]}'
         ur_comment2 = f'Answer2: {input_features[1]}'
@@ -52,7 +58,90 @@ def predict():
 
         output = [ur_comment1, ur_comment2, ur_comment3, testIE_result, testNS_result]
 
+
         return render_template('prediction.html', prediction_text = output )
 
 if __name__ == "__main__":
     app.run(threaded=True, debug=True, port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Use PyMongo to establish Mongo connection
+# # mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+# mongo = PyMongo(app, uri="mongodb+srv://hhos:Password1@cluster0-2fcii.mongodb.net/weather_data?retryWrites=true&w=majority")
+# # mongo = PyMongo(app, uri="mongodb://<dbuser>:<dbpassword>@ds231749.mlab.com:31749/heroku_ft9m418t&retryWrites=False")
+
+# print(mongo)
+
+
+# # Route to render index.html template using data from Mongo
+# @app.route("/livedata")
+# def livedata():
+
+#     # Find one record of data from the mongo database
+#     xxx_data = mongo.db.weather.find_one()  
+#     # Return template and data
+#     return render_template("livedata_heroku.html", weather=xxx_data) # 
+
+# @app.route("/")
+# def home():
+#     return render_template("alldata.html")
+                                      
+# @app.route("/getmygraph/<path:path>")
+# def send_bar(path):
+#     print(path)
+#     return send_from_directory('templates/', path) 
+
+# @app.route("/getmygraph2/<path:path>")
+# def send_bar2(path):
+#     print(path)
+#     return send_from_directory('templates/', path)
+
+# # @app.route("/getmygraph3/<path:path>")
+# # def url(path):
+# #     urlxxx = { 'urlicon':'http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' }
+# #     print(urlxxx['urlicon'])
+# #     return send_from_directory( urlxxx['urlicon'], path)
+
+
+# @app.route("/getmyfiles/<path:path>")
+# def send_js(path):
+#     print(path)
+#     return send_from_directory('templates/static/js/', path)
+
+
+
+# # Route that will trigger the scrape function
+# @app.route("/scrape")
+# def scrape():
+
+
+#     # Run the scrape function  the function in scrape_mars
+#     weather_data = scrape_aqiheroku.scrape()
+
+#     # Update the Mongo database using update and upsert=True  weather is the collection
+#     mongo.db.weather.update({}, weather_data, upsert=True)
+
+#     print("Mongo DB updated")
+
+#     # Redirect back to home page
+#     return redirect("/")
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port = 5000)
